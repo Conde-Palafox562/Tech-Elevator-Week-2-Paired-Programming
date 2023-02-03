f4b86489fc356fd48ea8af9fdd6ce82e6e804356
Raul Conde <r.conde2017@gmail.com>
2023-02-03 14:41:08 -0500

    Team Project Updates Part 2

--- exercise/src/main/java/com/techelevator/Employee.java
+++ exercise/src/main/java/com/techelevator/Employee.java
@@ -20,4 +20,78 @@ public class Employee {
         this.department = department;
         this.hireDate = hireDate;
     }
+
+    public Employee() {
+
+    }
+
+    public long getEmployeeID () {
+        return employeeID;
+    }
+
+    public String getFirstName () {
+        return firstName;
+    }
+
+    public String getLastName () {
+        return lastName;
+    }
+
+    public String getEmail () {
+        return email;
+    }
+
+    public double getSalary () {
+        return salary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public String getHireDate () {
+        return hireDate;
+    }
+
+    public void setEmployeeID (long employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public void setFirstName ( String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName ( String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail ( String email) {
+        this.email = email;
+    }
+
+    public void setSalary ( double salary) {
+        this.salary = salary;
+    }
+
+    public void setDepartment ( Department department) {
+        this.department = department;
+    }
+
+    public void setHireDate (String hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public String getFullName () {
+        return lastName + ". " + firstName;
+    }
+
+    public void getFullName (String lastName, String firstName) {
+        this.getFullName();
+    }
+
+    public void raisedSalary (double percent) {
+        this.salary = (salary * (percent / 100)) + salary;
+    }
+
+
 }
